c121ee46bd472b03c8a449c4df12fc7cd9aec6eb
Killgotka <vladaleksandrov1805@gmail.com>
2022-10-19 23:55:40 +0300

    BD

---
+++ package examples;.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Examples {
+
+    public static void main(String[] args) {
+        int some[] = new int[4];
+        ArrayList<Integer> some_din = new ArrayList<>(5);
+        some_din.add(56);
+        some_din.add(8);
+        some_din.add(7);
+        some_din.add(2);
+        some_din.add(3);
+        some_din.add(1);
+
+        for (Integer x : some_din) {
+            System.out.println(x);
+        }
+    }
+}
